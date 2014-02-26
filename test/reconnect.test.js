1d739a56f070c2cf8412b76f1dd740d1b32a0340
Jim Wang <jim.gs.wang@gmail.com>
2014-02-26 11:16:42 -0800

    add test cases for reconnect

--- test/reconnect.test.js
+++ test/reconnect.test.js
@@ -2,85 +2,33 @@ var sinon = require('sinon'),
     should = require('should'),
     reconnect = require('../lib/reconnect');
  
-describe.only('reconnect', function() {
+describe('reconnect', function() {
 
-    var options 
+    var stub,
+        spy;
 
     beforeEach(function() {
-        options = {
-            "tcp_timeout_increase" : 250,
-            "tcp_timeout_max" : 16000,
-            "http_timeout_base" : 5000,
-            "http_timeout_max" : 320000,
-            "http420_timeout_base" : 60000
+        stub = sinon.stub();
+        stub.time_to_wait = function() {
+            return 200;
         };
+        spy = sinon.spy();
+        this.clock = sinon.useFakeTimers();
     });
 
     afterEach(function() {
-        reconnect.reset();
+        this.clock.restore();
     });
 
-    describe('first restart attempt', function() {
-
-        beforeEach(function() {
-            this.clock = sinon.useFakeTimers();
-        });
-
-        afterEach(function() {
-            this.clock.restore();
-        });
-
-        it('should immediately restart on 200 status on first restart', function(done) {
-
-            reconnect.restart(200, options,  function() {
-                done();
-            });
-            this.clock.tick(1);
-        });
-
-        it('should immediately restart on 420 status', function(done) {
-
-            reconnect.restart(420, options, function() {
-                done();
-            });
-            this.clock.tick(1);
-
-        });
-
-        it('should immediately restart on other status', function(done) {
-
-            reconnect.restart(401, options, function() {
-                done();
-            });
-            this.clock.tick(1);
-
-        });
-
+    it('should not fire callback before strategy.time_to_wait()', function() {
+        reconnect.backoff(stub, spy);
+        this.clock.tick(199);
+        sinon.assert.notCalled(spy);
     });
 
-    describe('restart after attempting 2 restarts on 200 status', function() { 
-
-        beforeEach(function(done) {
-            reconnect.restart(200, options, function() {
-                reconnect.restart(200, options, function() {
-                    done();
-                    this.clock = sinon.useFakeTimers();
-                });
-            });
-        });
-
-        it('should not restart before 500ms', function(done) {
-
-            var spy = sinon.spy(done);
-
-            reconnect.restart(200, options, function() {
-                done();
-            });
-
-            this.clock.tick(499);
-
-            sinon.assert.notCalled(spy);
-            done();
-        });
+    it('should fire callback after strategy.time_to_wait()', function() {
+        reconnect.backoff(stub, spy);
+        this.clock.tick(200);
+        sinon.assert.called(spy);
     });
 });

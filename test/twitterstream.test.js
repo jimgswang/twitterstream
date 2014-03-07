9a9292de682ef360d1b0e6c5845d6b076e96a060
Jim Wang <jim.gs.wang@gmail.com>
2014-03-07 03:00:36 -0800

    refactor tests to not use nock

--- test/twitterstream.test.js
+++ test/twitterstream.test.js
@@ -1,10 +1,15 @@
 var sinon = require('sinon'),
     nock = require('nock'),
+    request = require('request'),
+    events = require('events'),
     TwitterStream = require('../lib/twitterstream');
 
 describe('twitterstream', function() {
 
-    var stream, scope;
+    var stream, scope,
+        emitter,
+        stub;
+
     beforeEach(function() {
         scope =  nock('https://stream.twitter.com')
                     .filteringPath(function(path) {
@@ -17,17 +22,38 @@ describe('twitterstream', function() {
             access_token: null,
             access_token_secret: null
         });
+
+        emitter = new events.EventEmitter();
+        stub = sinon.stub(request, 'post', function() {
+            return emitter;
+        });
+
+    });
+
+    afterEach(function() {
+        stub.restore();
     });
 
     describe('.stream', function() {
-        it.skip('should fire data event on successful response', function(done) {
-            scope.reply(200, {stuff : 'test'});
+        it('should fire data event when request receives data', function(done) {
+
             stream.on('data', function(data) {
                 done();
             });
             
             stream.stream({});
+            emitter.emit('data', 'test');
         });
 
+        it('should not fire data event when request receives \r\n keep alive', function(done) {
+
+            var spy = sinon.spy(done);
+            stream.on('data', spy);
+
+            stream.stream({});
+            emitter.emit('data', '\r\n');
+            sinon.assert.notCalled(spy);
+            done();
+        });
     });
 });
